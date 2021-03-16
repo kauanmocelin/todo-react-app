69bdd52299e80a3103be9f388490382a35ba3e98
kauanmocelin <kauanmocelin@gmail.com>
2021-03-16 09:29:15 -0300

    Layout responsivo para telas pequenas

--- src/components/TodoBuilder/style.js
+++ src/components/TodoBuilder/style.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 export const AppWrapper = styled.div`
   background-color: #423F5D;
   /* width: 40%; */
-  min-width: 450px;
+  min-width: 320px;
   height: 700px;
-  padding: 30px;
+  padding: 15px;
   box-sizing: border-box;
   border-radius: 5px;
   box-shadow: 0 15px 30px rgba(0, 0, 0, 0.4);
